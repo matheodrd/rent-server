3e090c5c161ddad53173c3e69ad12b8e4dea4764
Mathéo DURAND <matheodurand14@gmail.com>
2022-09-01 00:22:40 +0200

    prevent typing >max or <min

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 let pcores = document.querySelector("#pcores");
 let mcores = document.querySelector("#mcores");
 let inputCores = document.querySelector("#cores");
+const maxcores = inputCores.getAttribute("max");
 
+// increment button
 pcores.addEventListener("click", ()=>{
-    inputCores.value = parseInt(inputCores.value) + 1;
+    if (inputCores.value < maxcores) {
+        inputCores.value = parseInt(inputCores.value) + 1;
+    }
 });
 
+// decrement button
 mcores.addEventListener("click", ()=>{
     if (inputCores.value > 1) {
         inputCores.value = parseInt(inputCores.value) - 1;
     }
 });
+
+// prevent user from typing an int greater than max
+window.addEventListener('load', function() {
+    document.getElementById("cores").addEventListener('keyup', checkMax);
+});
+
+function checkMax(event) {
+    var value = this.value;
+
+    if (value > maxcores)
+        this.value = maxcores;
+}
