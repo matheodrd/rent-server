ceade547c26fb8639c98f2be4c4003dd645365a8
SKORPIA <99028310+SK0RPIA@users.noreply.github.com>
2022-09-02 14:09:58 +0200

    Modification SKORPIA

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 let pcores = document.querySelector("#pcores");
 let mcores = document.querySelector("#mcores");
 let inputCores = document.querySelector("#cores");
-const maxcores = 56;
 
+let pram = document.querySelector("#pram");
+let mram = document.querySelector("#mram");
+let inputRam = document.querySelector("#ram");
+const maxcores = 56;
+const maxram = 172;
 // increment button
 pcores.addEventListener("click", ()=>{
     if (inputCores.value < maxcores) {
         inputCores.value = parseInt(inputCores.value) + 1;
     }
-	console.log("inputCores");
 });
 
 // decrement button
@@ -16,18 +19,18 @@ mcores.addEventListener("click", ()=>{
     if (inputCores.value > 1) {
         inputCores.value = parseInt(inputCores.value) - 1;
     }
-	console.log("inputCores");
 });
 
 // prevent user from typing an int greater than max
-window.addEventListener('load', function() {
-    document.getElementById("cores").addEventListener('keyup', checkMax);
+window.addEventListener('load', ()=> {
+    document.getElementById("cores").addEventListener('keyup', checkMaxCpu);
+    document.getElementById("ram").addEventListener('keyup', checkMaxRam);
+
 });
 
-function checkMax(event) {
+function checkMaxCpu(event) {
     var value = this.value;
 	
-	console.log("maxcores");
 
     if (value > maxcores)
         this.value = maxcores;
@@ -35,3 +38,35 @@ function checkMax(event) {
 		this.value = 1;
 
 }
+
+function checkMaxRam(event) {
+    var value = this.value;
+	
+
+    if (value > maxram)
+        this.value = maxram;
+	else if (value == 0)
+		this.value = 1;
+
+}
+
+// increment button
+pram.addEventListener("click", ()=>{
+    if (inputRam.value < maxcores) {
+        inputRam.value = parseInt(inputRam.value) + 1;
+    }
+});
+
+// decrement button
+mram.addEventListener("click", ()=>{
+    if (inputRam.value > 1) {
+        inputRam.value = parseInt(inputRam.value) - 1;
+    }
+});
+
+inputRam.addEventListener("input", ()=>{
+    inputRam.value = !!inputRam.value && Math.abs(inputRam.value) >= 0 ? Math.abs(inputRam.value) : null
+});
+inputCores.addEventListener("input", ()=>{
+    inputCores.value = !!inputCores.value && Math.abs(inputCores.value) >= 0 ? Math.abs(inputCores.value) : null
+});
